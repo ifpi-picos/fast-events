40e0e77cf79cc94d9687669e11130780d49bddd2
mwenyo <mwenyo@gmail.com>
2022-03-29 09:41:53 -0300

    build: added swagger and helmet

---
+++ src/enumerators/profile-type.ts
@@ -0,0 +1,5 @@
+export enum ProfileType {
+  ADMIN = 1,
+  PARTCIPANT = 2,
+  PROMOTER = 3
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
+  const config = new DocumentBuilder()
+    .setTitle('Fast Events')
+    .setDescription('The Fast Events documentation API')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
