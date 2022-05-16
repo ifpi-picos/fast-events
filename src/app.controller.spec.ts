a0675e227c8668d0e0b0efb74a7da9107d2b70a4
jesielviana <jesielviana@pm.me>
2022-05-16 20:35:53 -0300

    fix tests and config deploy to heroku

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to the FAST EVENTS!"', () => {
+      expect(appController.getHello()).toBe('Welcome to the FAST EVENTS!');
     });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,22 @@
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '1000s' },
+        }),
+      ],
+      providers: [PrismaService, UsersService, AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
